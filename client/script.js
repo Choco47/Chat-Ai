a9ca9c1dc5bac87e7d87bdfaaa168af9c8471816
Choco47 <kelvin.chu1122@gmail.com>
2023-01-04 09:46:58 +0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   //fetch
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chat-ai-10fo.onrender.com', {
     method :'POST',
     headers: {
       'Content-Type': 'application/json'
